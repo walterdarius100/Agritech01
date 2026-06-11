a3c09670e9ba0229921bf6d152fc8af408df4f5b
walterdarius100 <121632552+walterdarius100@users.noreply.github.com>
2026-06-11 08:33:54 -0400

    Update supabase-config.js

--- js/config/supabase-config.js
+++ js/config/supabase-config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_CONFIG = {
   url: 'https://ujidzkzotlzyocozjyib.supabase.co/rest/v1/',
-  anonKey: 'sb_publishable_quk_TOcUEdRiiit5PFD83w_iMlU8_A9',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqaWR6a3pvdGx6eW9jb3pqeWliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExNTM2MDEsImV4cCI6MjA5NjcyOTYwMX0.ZNOYe_d25_Ns17OFTVBV-4WsiJsDAX_a8uDd1-PS6K0',
 };
